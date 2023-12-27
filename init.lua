c32daa69b174d762f0518c5250202e21291dde38
Mark <17348049+sockcrates@users.noreply.github.com>
2023-12-27 12:55:25 -0500

    Import custom plugins dir

--- init.lua
+++ init.lua
@@ -1020,8 +1020,5 @@ require('lazy').setup({
   },
 })
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
