dd6fcd3421898875dcb17d13f8fa16ac94cb1410
markg <17348049+sockcrates@users.noreply.github.com>
2025-02-05 11:43:54 -0500

    Add formatting and tab options for C

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -28,13 +28,17 @@ require('conform').formatters_by_ft.typescript = prettierThenEslint
 require('conform').formatters_by_ft.typescriptreact = prettierThenEslint
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'c', 'cpp' },
+  pattern = { 'c' },
   callback = function()
     vim.opt_local.tabstop = 2
     vim.opt_local.shiftwidth = 2
     vim.opt_local.softtabstop = 2
     vim.opt_local.expandtab = false
+    vim.opt_local.formatoptions = 'croq'
+    vim.opt_local.cindent = true
   end,
 })
+
+require('conform').formatters_by_ft.c = { 'clang-format' }
 
 return {}
