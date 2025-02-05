ae237180ab08a126fdf934012a1a2a17a44ed8d9
markg <17348049+sockcrates@users.noreply.github.com>
2025-02-05 08:58:58 -0500

    Add C and Cpp indent rules

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -27,4 +27,14 @@ require('conform').formatters_by_ft.typescriptreact = prettierThenEslint
 
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'c', 'cpp' },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = false
+  end,
+})
+
 return {}
